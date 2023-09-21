5b7a0de78a3755759ffc665e154765e7f918563c
Rose Pezzuti Dyer <pezzutidyer@uchicago.edu>
2023-09-21 15:17:48 -0500

    plot working, trying to get linear regression

--- mean-and-regression-line.py
+++ mean-and-regression-line.py
@@ -1,34 +1,38 @@
 import random, statistics
 import matplotlib.pyplot as plt
+from statistics import mean
 
 lowestcoord = 100
 highestcoord = 300
 lowestresidual = -5
 highestresidual = 5
 populationsize = 20
-slope = 2
+exampleslope = 2
 
 populationx = []
 populationy = []
 for i in range (0, populationsize):
 	x = random.randrange(lowestcoord, highestcoord)
 	residual = random.randrange(lowestresidual, highestresidual)
-	y = x * slope + residual
+	y = x * exampleslope + residual
 	populationx.append(x)
 	populationy.append(y)
-#print (populationx)
-#print (populationy)
+print (populationx)
+print (populationy)
+
+
+xmean = mean(populationx);
+ymean = mean(populationy);
+print ("x mean = ", xmean);
+print ("y mean = ", ymean);
+
+r = statistics.correlation(populationx, populationy)
+slope, intercept = statistics.linear_regression(populationx, populationy)
+
+predictedy = slope * xmean + intercept
+print ("Predicted y for ",xmean," = ", predictedy);
+print ("Is it ",ymean,"?");
 
 fig, ax = plt.subplots()
-
 ax.scatter(populationx, populationy)
-
-plt.show
-
-
-# x = [184, 163, 236, 148, 126, 278, 277, 238, 140, 250, 169, 210, 127, 109, 233, 139, 118, 175, 238, 142]
-# y = [364, 327, 473, 291, 255, 556, 554, 479, 276, 495, 334, 422, 256, 219, 463, 275, 240, 347, 477, 288]
-
-#fig, ax = plt.subplots()
-#ax.scatter(x, y)
-#plt.show()
+plt.show()
